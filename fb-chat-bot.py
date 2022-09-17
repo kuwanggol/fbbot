d29d45f77c5d7b690877883bdc3211b514906013
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-17 22:33:37 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -432,9 +432,10 @@ class ChatBot(Client):
                 self.sendRemoteFiles(
                     file_urls=final_link, message=None, thread_id=thread_id, thread_type=thread_type)
             elif(".image" in msg):
-                reply = "Searching..."
-                sendMsg()
-                imageSearch(self, msg)
+                if "credit:" not in msg:
+                    reply = "Searching..."
+                    sendMsg()
+                    imageSearch(self, msg)
 
             elif(".progsol" in msg):
                 programming_solution(self, msg)
