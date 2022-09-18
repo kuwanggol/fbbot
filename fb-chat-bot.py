3d54584fd91d9d89982391d70201a76bbc0c2b55
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-18 19:47:31 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -426,7 +426,7 @@ class ChatBot(Client):
                 ##reply = texttospeech(mytext)
                 sendMsg()
                 ##sendRemoteVoiceClips("https://www.mboxdrive.com/welcome.mp3", message=None, thread_id=None, thread_type=thread_type)
-                sendLocalVoiceClips("welcomuwwwue.mp3", message=None, thread_id=thread_id, thread_type=thread_type)
+                sendLocalVoiceClips("welcomuwwwue.mp3", message="Hi", thread_id=thread_id, thread_type=thread_type)
                 reply = "Working"
                 sendMsg()
 
