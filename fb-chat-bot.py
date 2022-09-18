f9a41cbc61edb6092bec00aeaf5044f6fce88d1d
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-18 17:30:38 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -471,8 +471,8 @@ class ChatBot(Client):
                 sendMsg()
             elif (".changenn" in msg):
                 msg = conSTR(msg,".changenn")
-                self.changeNickname(msg, user_id=joined_id, thread_id=thread_id, thread_type=thread_type)
-                
+                self.changeNickname(msg, user_id=None, thread_id=thread_id, thread_type=thread_type)
+
             #reply = msg;
             #sendMsg()
 
