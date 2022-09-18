f2c08a560e5508178f5febcf044009c4cb500fc0
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-19 05:24:27 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -590,7 +590,8 @@ class ChatBot(Client):
             self.onPersonRemoved(mid=mid,removed_id=removed_id,author_id=author_id,thread_id=thread_id,ts=ts,msg=delta)
             self.addUsersToGroup(removed_id, thread_id=thread_id)
             reply = "Bawal mag leave ✌️😎"
-            sendMsg()
+            self.send(Message(text=reply), thread_id=thread_id,
+                  thread_type=ThreadType.USER)
 
     def onColorChange(self, mid=None, author_id=None, new_color=None, thread_id=None, thread_type=ThreadType.USER, **kwargs):
         reply = "You changed the theme ✌️😎"
