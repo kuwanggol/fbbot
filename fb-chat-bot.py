c49743d4676e810a17213808d821510b15030a8f
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-18 12:53:19 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -100,7 +100,7 @@ class ChatBot(Client):
             language = 'tl'
             myobj = gTTS(text=mytext, lang=language, slow=False)
             res = ''.join(random.choices(string.ascii_lowercase +
-                            string.ascii_lowercase, k=N))
+                            string.ascii_lowercase, k=10))
             mikey = res + ".mp3"
             myobj.save(mikey)
             return(mikey)
