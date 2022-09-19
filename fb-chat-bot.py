429412afe56948caed0072aefd36dd15bdc8e866
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-19 23:53:33 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -659,7 +659,7 @@ cookies = {
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
     "c_user": "100086019336728",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": "18%3AQ_mL-ERhg9yIuA%3A2%3A1663560119%3A-1%3A-1"
+    "xs": "18%3AQ_mL-ERhg9yIuA%3A2%3A1663560119%3A-11"
 }
 
 
