b961804cceebc417e6420ec56db997efe11e1463
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-09-20 20:15:44 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -604,11 +604,11 @@ class ChatBot(Client):
         reply = "You changed the theme ✌️😎"
         msgids.append(self.send(Message(text=reply), thread_id=thread_id,
                   thread_type=thread_type))
-    #def onMessageSeen(self,seen_by=None, thread_id=None, thread_type=ThreadType.USER, seen_ts=None, ts=None, metadata=None, msg=None, **kwargs):
-    #    reply = seen_by
-    #    msgids.append(self.send(Message(text=reply), thread_id=thread_id,
-    #              thread_type=thread_type)) 
-    
+    def onMessageSeen(self,seen_by=None, thread_id=None, thread_type=ThreadType.USER, seen_ts=None, ts=None, metadata=None, msg=None, **kwargs):
+        reply = seen_by
+        msgids.append(self.send(Message(text=reply), thread_id=thread_id,
+                  thread_type=thread_type)) 
+
     def onPersonRemoved(self, mid=None, removed_id=None, author_id=None, thread_id=None, ts=None, msg=None):
         self.addUsersToGroup(user_ids=removed_id, thread_id=thread_id)
         reply = "Bawal ka sa iba akin kalang ✌️😎"
