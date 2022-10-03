6e31603bf6282a39e7d2363b6d1a49c95c0ff415
kuwanggol <109716973+kuwanggol@users.noreply.github.com>
2022-10-03 12:17:29 +0800

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -579,7 +579,7 @@ class ChatBot(Client):
                 sendMsg()
                 texttospeech(reply)
             elif ("mikeyy" == msg):
-                reply = self.fetchUserInfo(100086019336728)
+                reply = self.fetchUserInfo("100086019336728")["100086019336728"]
                 sendMsg()
             #self.changeNickname("Bot", user_id=100086019336728, thread_id=thread_id, thread_type=thread_type)
             #reply = msg;
